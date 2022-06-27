2cc8ebc04bfcd14f4541c960db36d29e0a2a5dd3
VladimirDip <kent4594111@gmail.com>
2022-06-28 06:27:23 +1000

    added some new algoritms

---
+++ 5. Final sorting algoritms tasks/A. Largest Number.py
@@ -0,0 +1,16 @@
+class Comparator:
+    def __init__(self, number):
+        self.number = number
+
+    def __lt__(self, other):
+        return self.number + other.number > other.number + self.number
+
+
+def make_largest_number(numbers):
+    return ''.join(sorted(numbers, key=Comparator))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    numbers_list = input().split()
+    print(make_largest_number(numbers_list))
